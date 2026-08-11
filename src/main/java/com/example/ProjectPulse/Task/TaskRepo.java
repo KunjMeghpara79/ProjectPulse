41427b9ae041397c46b738179c1cdf55bdc57dd7
Kunj Meghapara <kunjmeghapara777@gmail.com>
2026-08-11 11:37:41 +0530

    ..

--- src/main/java/com/example/ProjectPulse/Task/TaskRepo.java
+++ src/main/java/com/example/ProjectPulse/Task/TaskRepo.java
@@ -14,4 +14,11 @@ public interface TaskRepo extends JpaRepository<Task,Integer> {
     @Modifying
     @Query("Delete from Task t where t.employeeId = :employeeId")
     void deleteTasks(@Param("employeeId") int employeeId);
+
+    /*
+    this method will be executed when the employee will be deleted
+    so their tasks will also be deleted so we have to go through all projects in which the particular
+    employee exists and from that project object delete the tasks which contains that employeeId.
+    by using this query method we can directly delete the tasks from tasks table
+     */
 }
